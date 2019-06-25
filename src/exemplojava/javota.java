af5df67a076a4da47548ca59ed518b6d9394bfb6
brunobarboosa <brunosantos.2004@outlook.com>
2019-06-25 12:16:09 -0300

    popo

---
+++ src/exemplojava/javota.java
@@ -0,0 +1,18 @@
+package exemplojava;
+
+public class javota {
+	
+	 public static void main(String[] args) {
+		 char a = 'a';
+		 int i = 0;
+		 float f = 10.3f;
+		 double d = 10;
+		 
+		 System.out.println("a = " + a);
+		 System.out.println("i = " + i);
+		 System.out.println("f = " + f);
+		 System.out.println("d = " + d);
+		 
+	 }
+
+}
